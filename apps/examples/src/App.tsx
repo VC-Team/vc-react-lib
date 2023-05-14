8083bcc596809a4f5afd6717b69b61126aebd2d4
gialynguyen(Steven) <emthanchet@gmail.com>
2023-05-14 22:47:06 +0700

    Training day 4

--- apps/examples/src/App.tsx
+++ apps/examples/src/App.tsx
@@ -3,9 +3,21 @@ import { Tooltip } from "vc-react-tooltip";
 function App() {
   return (
     <div style={{ margin: 100 }}>
-      <Tooltip>VC-Team</Tooltip>
+      <Tooltip placement="right" trigger={["hover"]} title="Popover">
+        <span style={{ border: "1px solid red" }}>
+          VC-Team is an open source team
+        </span>
+      </Tooltip>
     </div>
   );
 }
 
 export default App;
+
+// - Tìm hiểu thêm về rule của children (content node). Tại sao nó phải là 1 HTML Element hoặc 1 forwardRef
+// - Implement 3 props: placement, trigger, title
+
+// What next?
+// - Implement hook onClickOutSide
+// - Remove findDomNode
+// - CSS in JS: Emotion
--- packages/vc-react-tooltip/src/Tooltip.tsx
+++ packages/vc-react-tooltip/src/Tooltip.tsx
@@ -1,94 +1,138 @@
 import {
-  Component,
-  MutableRefObject,
   PropsWithChildren,
   ReactNode,
+  useCallback,
   useEffect,
   useRef,
 } from "react";
-import { createPortal, findDOMNode } from "react-dom";
+import { createPortal } from "react-dom";
+import { FindDomNode } from "./FindDomNode";
 
-export type TooltipProps = PropsWithChildren<{}>;
+type TooltipPlacement = "top" | "left" | "right" | "bottom";
 
-type FindDomNodeProps = PropsWithChildren<{
-  innerRef: MutableRefObject<{
-    getDomNode: () => HTMLElement;
-  } | null>;
+type TooltipTrigger = ("hover" | "focus" | "click")[];
+
+type TooltipTitle = ReactNode | (() => ReactNode);
+
+export type TooltipProps = PropsWithChildren<{
+  placement: TooltipPlacement;
+  trigger: TooltipTrigger;
+  title: TooltipTitle;
 }>;
 
-class FindDomNode extends Component<FindDomNodeProps> {
-  constructor(props: PropsWithChildren<FindDomNodeProps>) {
-    super(props);
-  }
-
-  componentDidMount(): void {
-    const node = findDOMNode(this);
-    if (node instanceof HTMLElement) {
-      this.props.innerRef.current = {
-        getDomNode() {
-          return node;
-        },
-      };
-    }
-  }
-
-  render(): ReactNode {
-    return this.props.children;
-  }
-}
-
-export const Tooltip = ({ children }: TooltipProps) => {
+export const Tooltip = ({
+  children,
+  placement,
+  trigger,
+  title,
+}: TooltipProps) => {
   const popoverContainerRef = useRef(document.createElement("div"));
   const contentInnerRef = useRef<{
     getDomNode: () => HTMLElement;
   } | null>(null);
+
   const popoverElementRef = useRef<HTMLDivElement | null>(null);
 
   if (!document.body.contains(popoverContainerRef.current)) {
     document.body.append(popoverContainerRef.current);
   }
 
-  useEffect(() => {
-    if (contentInnerRef.current) {
-      const node = contentInnerRef.current.getDomNode();
+  const displayPopover = useCallback(() => {
+    const node = contentInnerRef.current?.getDomNode();
+    const popoverElement = popoverElementRef.current;
+    if (!node || !popoverElement) return;
+    const rect = node.getBoundingClientRect();
 
-      const contentOnMouseEnter = () => {
-        const popoverElement = popoverElementRef.current;
-        const rect = node.getBoundingClientRect();
+    const isDisplay = popoverElement.style.display === "block";
+    if (isDisplay) return;
 
-        if (popoverElement) {
-          popoverElement.style.display = "block";
-          popoverElement.style.left = `${rect.x}px`;
-          popoverElement.style.top = `${rect.y + rect.height}px`;
-        }
-      };
+    popoverElement.style.display = "block";
 
-      const contentOnMouseLeave = () => {
-        const popoverElement = popoverElementRef.current;
-        if (popoverElement) {
-          popoverElement.style.display = "none";
-        }
-      };
+    switch (placement) {
+      case "top":
+        popoverElement.style.left = `${
+          rect.x + rect.width / 2 - popoverElement.clientWidth / 2
+        }px`;
+        popoverElement.style.top = `${rect.y - rect.height}px`;
+        break;
+      case "bottom":
+        popoverElement.style.left = `${
+          rect.x + rect.width / 2 - popoverElement.clientWidth / 2
+        }px`;
+        popoverElement.style.top = `${rect.y + rect.height}px`;
+        break;
+      case "left":
+        popoverElement.style.left = `${rect.x - popoverElement.clientWidth}px`;
+        popoverElement.style.top = `${
+          rect.y + rect.height / 2 - popoverElement.clientHeight / 2
+        }px`;
+        break;
+      case "right":
+        popoverElement.style.left = `${rect.x + rect.width}px`;
+        popoverElement.style.top = `${
+          rect.y + rect.height / 2 - popoverElement.clientHeight / 2
+        }px`;
+        break;
+      default:
+        break;
+    }
+  }, [placement]);
 
-      node.addEventListener("mouseenter", contentOnMouseEnter);
-      node.addEventListener("mouseleave", contentOnMouseLeave);
+  const hidePopover = useCallback(() => {
+    const node = contentInnerRef.current?.getDomNode();
+    const popoverElement = popoverElementRef.current;
+    if (!node || !popoverElement) return;
+
+    popoverElement.style.display = "none";
+  }, []);
+
+  useEffect(
+    function triggerContentEvent() {
+      const node = contentInnerRef.current?.getDomNode();
+      if (!node) return;
+
+      const cleanups: (() => void)[] = [];
+
+      if (trigger.includes("hover")) {
+        node.addEventListener("mouseenter", displayPopover);
+
+        node.addEventListener("mouseleave", hidePopover);
+
+        cleanups.push(() => {
+          node.removeEventListener("mouseenter", displayPopover);
+          node.removeEventListener("mouseleave", hidePopover);
+        });
+      }
+
+      if (trigger.includes("click")) {
+        node.addEventListener("click", displayPopover);
+
+        // Click outside to hide popover
+
+        cleanups.push(() => {
+          node.removeEventListener("click", displayPopover);
+
+          // Remove click outside event
+        });
+      }
 
       return () => {
-        node.removeEventListener("mouseenter", contentOnMouseEnter);
-        node.removeEventListener("mouseleave", contentOnMouseLeave);
+        cleanups.forEach((cleanup) => cleanup());
       };
-    }
-  }, []);
+    },
+    [...trigger, displayPopover, hidePopover]
+  );
 
   return (
     <>
       <FindDomNode innerRef={contentInnerRef}>{children}</FindDomNode>
       {createPortal(
         <div
+          className="vc-tooltip"
           ref={popoverElementRef}
           style={{ position: "absolute", display: "none" }}
         >
-          Popover
+          {typeof title === "function" ? title() : title}
         </div>,
         popoverContainerRef.current
       )}
